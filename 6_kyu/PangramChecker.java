5a070a3af998063b42c8264624d6975c6bba7460
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-10-10 19:47:01 +0300

    Create PangramChecker.java

---
+++ 6_kyu/PangramChecker.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PangramChecker {
+  public boolean check(String s){
+    
+    char[] f = s.toLowerCase().replace(" ","").toCharArray();
+    
+    Set<Character> pool = new HashSet<>();
+    
+    for(int i = 0; i<=f.length-1;i++){
+      
+     for(char c ='a';c<='w';c++){
+       
+       if(f[i]==c)pool.add(c);
+       
+     }
+      
+    }
+    
+    return pool.size() >= 23 ? true: false;
+    
+    }
+}
