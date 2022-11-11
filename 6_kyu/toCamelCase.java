5bacb6e27dfe6d3261ec962de917853791888b84
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-11-11 22:57:10 +0200

    Update toCamelCase.java

--- 6_kyu/toCamelCase.java
+++ 6_kyu/toCamelCase.java
@@ -5,7 +5,8 @@ class Solution{
   static String toCamelCase(String s){
     
     String[] c = s.split("-|\\_");
-    for(int i = 1; i < c.length;i++){c[i] =  String.valueOf(Character.toUpperCase((c[i].charAt(0))))+ c[i].substring(1)};
+    
+    for(int i = 1; i < c.length ; i++) { c[i] =  String.valueOf ( Character.toUpperCase( ( c[i].charAt(0) ) ) ) + c[i].substring(1) };
     
     return  new String(String.valueOf(Arrays.toString(c)).replaceAll("]|\\[|\\ |\\_|\\-|\\,",""));
     
