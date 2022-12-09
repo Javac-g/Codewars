b7d2577c07740c9386b4bd12010853f6a5129654
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-12-09 20:41:30 +0200

    Update PigLatin.java
    Still mess with spec chars

--- 5_kyu/PigLatin.java
+++ 5_kyu/PigLatin.java
@@ -7,7 +7,7 @@ public class PigLatin {
         String[] word = str.split(" ");
       
         for(int i = 0;i <word.length;i++){
-          word[i]=word[i].substring(1)+ word[i].substring(0,1).replaceAll("!","") ;
+          word[i]=word[i].substring(1)+ word[i].substring(0,1) ;
           if(word[i].length()>0){
              word[i]+= "ay";
          }
