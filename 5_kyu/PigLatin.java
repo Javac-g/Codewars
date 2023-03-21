d0e228d33aee050988c3f0bcd2bc2ff86c53fe27
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-03-21 21:38:36 +0200

    Update PigLatin.java

--- 5_kyu/PigLatin.java
+++ 5_kyu/PigLatin.java
@@ -13,5 +13,16 @@ public class PigLatin {
          }
        }   
       return new String(String.valueOf(Arrays.toString(word)).replaceAll("]|\\[|\\_|\\-|\\,",""));
+    } public static String pigIt(String str) {
+        System.out.println(str);
+        String[] word = str.split(" ");
+      
+        for(int i = 0;i <word.length;i++){
+          word[i]=word[i].substring(1)+ word[i].substring(0,1) ;
+          if(word[i].length()>0){
+             word[i]+= "ay";
+         }
+       }   
+      return new String(String.valueOf(Arrays.toString(word)).replaceAll("]|\\[|\\_|\\-|\\,",""));
     }
 }
