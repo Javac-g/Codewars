9385d8b3f81ac5b9b5f7b1eb5cd7af2619a7d270
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-04-01 23:40:04 +0300

    Update PigLatin.java

--- 5_kyu/PigLatin.java
+++ 5_kyu/PigLatin.java
@@ -5,7 +5,7 @@ public class PigLatin {
     public static String pigIt(String str) {
         System.out.println(str);
         String[] word = str.split(" ");
-      
+      String one;
         for(int i = 0;i <word.length;i++){
           word[i]=word[i].substring(1)+ word[i].substring(0,1) ;
           if(word[i].length()>0){
