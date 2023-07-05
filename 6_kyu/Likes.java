ef38cd1fe63c92fc8c7baaa4627e524cd64d2939
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-07-05 23:48:22 +0300

    Update Likes.java

--- 6_kyu/Likes.java
+++ 6_kyu/Likes.java
@@ -3,7 +3,7 @@ class Likes {
         switch (nms.length) {
           case 0: return "nobody  this";
           case 1: return String.format("%s likes this", nms[0]);
-          case 2: return String.format("%s and %s like this", nms[0], nms[1]);
+          case 2: return String.format("%s and %s likes this", nms[0], nms[1]);
           case 3: return String.format("%s, %s and %s like this", nms[0], nms[1], nms[2]);
           default: return String.format("%s, %s and %d others like this", nms[0], nms[1], nms.length - 2);
         }
