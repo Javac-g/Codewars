69669fd3357fd429c5ed051008c52559e760c6c9
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-07-23 22:15:23 +0300

    Update StrongNumber2.java

--- 7_kyu/StrongNumber2.java
+++ 7_kyu/StrongNumber2.java
@@ -1,7 +1,7 @@
 import static java.util.stream.IntStream.rangeClosed;
 
 class StrongNumber {
-  static String isStrongNumber(int num) {
+ final static String isStrongNumber(int num) {
     return (num + "").chars().map(Character::getNumericValue)
                      .map(i -> rangeClosed(1, i).reduce(1, (a, b) -> a * b))
                      .sum() == num ? "STRONG!!!!" : "Not Strong !!";
