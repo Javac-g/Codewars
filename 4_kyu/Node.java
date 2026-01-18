11605257c43e94d6eab652ad2fe2c7ba43684f2d
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2026-01-18 23:36:20 +0200

    Create Node class for binary tree structure

---
+++ 4_kyu/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+  public Node left;
+  public Node right;
+  public int value;
+  
+  public Node(Node l, Node r, int v) {
+    left = l;
+    right = r;
+    value = v;
+  }
+}
